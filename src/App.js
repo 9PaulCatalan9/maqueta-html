985a9bcc2635958a96bec6eba0697790ca70b6b0
9PaulCatalan9 <23002255@galileo.edu>
2024-04-23 00:05:42 -0600

    segundo commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Carta from './Componentes/Carta/Carta'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export function App(props) {
 
   return (
