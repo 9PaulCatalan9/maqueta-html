ff4a7697e838f534e5cd919ce682595e6f6127bc
9PaulCatalan9 <23002255@galileo.edu>
2024-04-29 00:33:16 -0600

    cuarto commit

---
+++ src/Componentes/AddBtn/AddBtn.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import './AddBtn.scss';
+function AddBtn() {
+  return (
+    <>
+      <Button variant="primary" id='btn-add'>Añadir meta</Button>
+
+    </>
+  );
+}
+
+export default AddBtn;
